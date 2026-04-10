6a2048869a8d0810f59a6a72631fbda913211d6d
PK (mac) <monpushp@gmail.com>
2026-04-10 18:21:03 +0530

    Changes to be committed: 	modified:   src/renderer/index.html 	modified:   src/renderer/src/assets/main.css 	modified:   src/renderer/src/main.jsx 	modified:   src/renderer/src/pages/Activation.jsx

--- src/renderer/src/main.jsx
+++ src/renderer/src/main.jsx
@@ -2,9 +2,7 @@ import 'primereact/resources/themes/saga-blue/theme.css';
 import 'primereact/resources/primereact.min.css';
 import 'primeicons/primeicons.css';
 import 'primeflex/primeflex.css';
-
 import './assets/main.css';
-
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
--- src/renderer/src/pages/Activation.jsx
+++ src/renderer/src/pages/Activation.jsx
@@ -7,6 +7,7 @@ import { Ripple } from 'primereact/ripple';
 import { Image } from 'primereact/image';
 import { useApi } from '../hooks/useApi';
 import lslogo from '../assets/leapsys-inkspace-on-transparent.png';
+import { Panel } from 'primereact/panel';
 
 Activation.propTypes = {
   onProceed: PropTypes.func.isRequired,
@@ -20,27 +21,65 @@ export default function Activation({ onProceed, onBack }) {
 
   const startContent = (
     <React.Fragment>
-      <span style={{ textAlign: 'center', marginTop: 0, fontWeight: 900 }}>Activation</span>
+      <span>Activation</span>
     </React.Fragment>
   );
 
   const endContent = (
     <React.Fragment>
-      <Button label="Back" onClick={() => onBack()} disabled={true} visible={false} className="p-button-plain p-button-danger mr-2 p-3" />
-      <Button label="Demo" onClick={() => onProceed()} visible={false} className="p-button-secondary p-3 mr-2" />
-      <Button label="Activate Now" onClick={() => handleActivate()} className="p-button-success p-3" />
+      <Button label="Back" onClick={() => onBack()} disabled={true} visible={false} className="p-button-plain p-button-danger m-0 p-3 mr-1" />
+      <Button label="Demo" onClick={() => onProceed()} visible={false} className="p-button-secondary p-3 mr-1" />
+      <Button label="Activate" onClick={() => handleActivate()} className="p-button-success p-3" />
     </React.Fragment>
   );
 
   return (
     <>
-      <div>
+      {/* <div>
         <Toolbar start={startContent} end={endContent} className="shadow-1" style={{ backgroundImage: 'linear-gradient(to left, var(--bluegray-100), var(--bluegray-300))' }} />
         <div style={{ border: '0px solid #888', marginTop: '10px' }}>
           <div className="flex flex-column align-items-center justify-content-center">
             <span>Ready for activation !!</span>
           </div>
         </div>
+      </div> */}
+
+      {/* <Panel header="Header" className="h-screen">
+        <p className="m-0">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum.
+        </p>
+      </Panel> */}
+
+      <div className="flex flex-column min-h-screen">
+        <header className="p-0 bg-primary text-white">
+          <Toolbar start={startContent} end={endContent} style={{ backgroundImage: 'linear-gradient(to left, var(--blue-50), var(--blue-600))' }} />
+        </header>
+
+        <main className="flex-1 p-0">
+          <div className="surface-card p-6 h-full">
+            <h3>Activation Pending</h3>
+            <p>Once you click it may take couple of minutes to activate.</p>
+          </div>
+        </main>
+
+        <footer className="p-0 bg-blue-600 text-white p-1">
+          <div className="flex align-items-center justify-content-between">
+            <div>
+              <small>© 2026 LEAPSYS</small>
+            </div>
+
+            <div className="flex align-items-center gap-3">
+              <i className="pi pi-wifi"></i>
+              <i className="pi pi-cog"></i>
+              <i className="pi pi-globe"></i>
+              <span className="flex align-items-center gap-2 pr-3">
+                <i className="pi pi-circle-fill text-red-400" style={{ fontSize: '0.6rem' }}></i>
+                <small>Offline</small>
+              </span>
+            </div>
+          </div>
+        </footer>
       </div>
     </>
   );
