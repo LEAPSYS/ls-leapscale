123e8592c6e3e3304ea36fcaa0cbeadb0047e176
PK (mac) <monpushp@gmail.com>
2026-06-16 16:56:19 +0530

    Changes to be committed: 	modified:   src/renderer/src/services/axiosService.js

--- src/renderer/src/services/axiosService.js
+++ src/renderer/src/services/axiosService.js
@@ -35,8 +35,8 @@ backendApiClient.interceptors.request.use(
 
 const jupiterApiClient = axios.create({
   // baseURL: 'https://service.leapsys.in/jupiter-prd',
-  // baseURL: 'https://service.leapsys.in/jupiter-uat',
-  baseURL: 'http://localhost:8888/jupiter-local',
+  baseURL: 'https://service.leapsys.in/jupiter-uat',
+  // baseURL: 'http://localhost:8888/jupiter-local',
   timeout: 30000,
   headers: {
     'Content-Type': 'application/json'
